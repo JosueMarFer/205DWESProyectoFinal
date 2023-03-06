74ecc52e49e6cec1dc20e1976583e435908b154b
josueMarFer <josuebenavente2018@gmail.com>
2023-03-06 12:13:45 +0100

    01/03/2023
    Fix

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -6,14 +6,14 @@
 //const PASSWD = 'paso';
 //const DATABASE = 'DB205DWESProyectoFinal';
 //Conexion Servidor Desarrollo Clase
-const HOST = '192.168.20.19';
-const HOSTPDO = 'mysql:host=192.168.20.19; dbname=DB205DWESProyectoFinal';
-const USER = 'user205DWESProyectoFinal';
-const PASSWD = 'paso';
-const DATABASE = 'DB205DWESProyectoFinal';
+//const HOST = '192.168.20.19';
+//const HOSTPDO = 'mysql:host=192.168.20.19; dbname=DB205DWESProyectoFinal';
+//const USER = 'user205DWESProyectoFinal';
+//const PASSWD = 'paso';
+//const DATABASE = 'DB205DWESProyectoFinal';
 //Conexion Servidor Explotación
-//const HOST = 'db5010845872.hosting-data.io';
-//const HOSTPDO = 'mysql:host=db5010845872.hosting-data.io; dbname=dbs9174047;';
-//const USER = 'dbu2082397';
-//const PASSWD = 'daw2_Sauces';
-//const DATABASE = 'dbs9174047';
+const HOST = 'db5010845872.hosting-data.io';
+const HOSTPDO = 'mysql:host=db5010845872.hosting-data.io; dbname=dbs9174047;';
+const USER = 'dbu2082397';
+const PASSWD = 'daw2_Sauces';
+const DATABASE = 'dbs9174047';
